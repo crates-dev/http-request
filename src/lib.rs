3f06d93a74ab629bb39af62b6a8777de4edd102a
尤雨东 <root@ltpp.vip>
2026-01-21 21:34:49 +0800

    feat: v8.91.82

--- src/lib.rs
+++ src/lib.rs
@@ -18,11 +18,13 @@ mod utils;
 
 pub use {request::*, response::*};
 
-pub use http_type::{HashMapXxHash3_64, RequestError, hash_map_xx_hash3_64};
-pub use serde_json::{
-    Deserializer, Error, Map, Number, StreamDeserializer, Value, from_reader, from_slice, from_str,
-    from_value, to_string, to_string_pretty, to_value, to_vec, to_vec_pretty, to_writer,
-    to_writer_pretty, value,
+pub use {
+    http_type::{HashMapXxHash3_64, RequestError, hash_map_xx_hash3_64},
+    serde_json::{
+        Deserializer, Error, Map, Number, StreamDeserializer, Value, from_reader, from_slice,
+        from_str, from_value, to_string, to_string_pretty, to_value, to_vec, to_vec_pretty,
+        to_writer, to_writer_pretty, value,
+    },
 };
 
 use {common::*, utils::*};
@@ -50,31 +52,31 @@ use std::{
     time::Instant,
 };
 
-use futures::{Future, Sink, SinkExt, Stream, StreamExt};
-#[cfg(test)]
-use http_type::tokio;
-use http_type::{
-    ACCEPT, ACCEPT_ANY, BR_BYTES, COLON_U8, CONNECTION, CONTENT_LENGTH, CONTENT_TYPE, Compress,
-    ContentType, DEFAULT_BUFFER_SIZE, DEFAULT_HTTP_PATH, DEFAULT_MAX_REDIRECT_TIMES,
-    DEFAULT_TIMEOUT, EMPTY_STR, HOST, HTTP_BR_BYTES, HttpStatus, HttpUrlComponents, HttpVersion,
-    LOCATION, Method, Protocol, QUERY, RequestBody, RequestBodyString, RequestHeaders,
-    ResponseHeaders, ResponseStatusCode, SEC_WEBSOCKET_KEY, SEC_WEBSOCKET_VERSION, SPACE_U8,
-    TAB_U8, UPGRADE, USER_AGENT,
-    tokio::{
-        io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, ReadBuf},
-        net::TcpStream as AsyncTcpStream,
-        runtime::Runtime,
-        sync::{Mutex as AsyncMutex, MutexGuard as AsyncMutexGuard},
-        time::timeout,
+use {
+    futures::{Future, Sink, SinkExt, Stream, StreamExt},
+    http_type::{
+        ACCEPT, ACCEPT_ANY, BR_BYTES, COLON_U8, CONNECTION, CONTENT_LENGTH, CONTENT_TYPE, Compress,
+        ContentType, DEFAULT_BUFFER_SIZE, DEFAULT_HTTP_PATH, DEFAULT_MAX_REDIRECT_TIMES,
+        DEFAULT_TIMEOUT, EMPTY_STR, HOST, HTTP_BR_BYTES, HttpStatus, HttpUrlComponents,
+        HttpVersion, LOCATION, Method, Protocol, QUERY, RequestBody, RequestBodyString,
+        RequestHeaders, ResponseHeaders, ResponseStatusCode, SEC_WEBSOCKET_KEY,
+        SEC_WEBSOCKET_VERSION, SPACE_U8, TAB_U8, UPGRADE, USER_AGENT,
+        tokio::{
+            io::{AsyncRead, AsyncReadExt, AsyncWrite, AsyncWriteExt, ReadBuf},
+            net::TcpStream as AsyncTcpStream,
+            runtime::Runtime,
+            sync::{Mutex as AsyncMutex, MutexGuard as AsyncMutexGuard},
+            time::timeout,
+        },
     },
+    rustls::{ClientConfig, ClientConnection, RootCertStore, StreamOwned, pki_types::ServerName},
+    serde::{Serialize, Serializer},
+    tokio_rustls::{TlsConnector, client::TlsStream},
+    tokio_tungstenite::{
+        MaybeTlsStream, WebSocketStream, client_async_with_config, connect_async_with_config,
+        tungstenite::Message, tungstenite::handshake::client::Request,
+    },
+    webpki_roots::TLS_SERVER_ROOTS,
 };
-use rustls::{ClientConfig, ClientConnection, RootCertStore, StreamOwned, pki_types::ServerName};
-use serde::{Serialize, Serializer};
 #[cfg(test)]
-use serde_json::json;
-use tokio_rustls::{TlsConnector, client::TlsStream};
-use tokio_tungstenite::{
-    MaybeTlsStream, WebSocketStream, client_async_with_config, connect_async_with_config,
-    tungstenite::Message, tungstenite::handshake::client::Request,
-};
-use webpki_roots::TLS_SERVER_ROOTS;
+use {http_type::tokio, serde_json::json};
